874e802dcb32072601dfab41ee7b76a02d8ae101
Nathan-Payne <paynee89@yahoo.co.uk>
2020-12-10 21:11:57 +0000

    initial nuxt set up, install sass dependency, include logo svg as components, configure code formatters, remove boilerplate nuxt content

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div>
+      <TxoLogo />
+      <h1 class="">
+        We’re hands-on and committed to evolving your business to strive for
+        better.
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import TxoLogo from '~/components/logos/txo-logo'
+
+export default {
+  components: {
+    TxoLogo,
+  },
+}
+</script>
+
+<style></style>
